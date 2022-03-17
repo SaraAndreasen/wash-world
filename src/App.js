cff61b5ae0075db95784fff3d130f5482200d087
SaraAndreasen <sanijuni@hotmail.com>
2022-03-17 15:29:47 +0100

    start of assignment
    all code updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import info from "./info";
+import WashLocation from "./components/WashLocation";
 
 function App() {
+  const [locations, setLocations] = useState([]);
+
+  /** get er axios specific, hvis man ikke bruger axios, er det fetch. Det vi får når vi bruger .then er vores response. Med axios skal du ikke lave det om JSON, men i fetch skal du selv lave det om til JSON */
   useEffect(() => {
-    axios.get(info.backendUrl + "/locations");
+    axios.get(info.backendUrl + "/locations").then((result) => {
+      setLocations(result.data.response.locations);
+    });
   }, []);
-  return <div></div>;
+
+  return (
+    <div>
+      {locations.map((location) => {
+        return <WashLocation key={location.id} location={location} />;
+      })}
+    </div>
+  );
 }
 
 export default App;
